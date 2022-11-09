f61a2f86a27287a226bbb5a801a405d5fe975b57
gizempesen <pesengizem@gmail.com>
2022-11-09 10:47:14 +0000

    terraform run

---
+++ terraform-install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install wget curl unzip software-properties-common gnupg2 -y
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update -y
+sudo apt-get install terraform -y
+
